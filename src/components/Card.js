1e8d25734b197b802a86b3b6c3487701a92161e9
-g <-g>
2024-11-04 13:05:58 +0530

    card changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ function Card() {
     <div className="container">
       <p className="cardheader">Card Title</p>
       <p className="subheader">optional Subheader</p>
-
+      <p>Heaser(optional)</p>
       <p>
         Jsx shorthand for javascript XMl.This type of file is use in react which
         utilizes the expressiveness of javasript along with html template syntax
